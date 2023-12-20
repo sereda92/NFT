48a2b63d224c28345aade2df3b4d1dfb23ebbd4b
sereda92 <assakay92@gmail.com>
2023-12-20 16:00:46 +0200

    burger

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 // const textPath = document.querySelector('textPath');
 // textPath.setAttribute('startOffset', '0%'); // Начало текста с середины круга
+
+const burgerBody = document.getElementById('burger-body')
+const burgerBtn = document.getElementById('burgerBtn')
+
+burgerBtn.addEventListener('click', function(){
+    if(burgerBody.classList.contains('burger-active')){
+        burgerBody.classList.remove('burger-active')
+    }else{
+        burgerBody.classList.add('burger-active')
+    }
+
+})
