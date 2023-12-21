b2361266145d72c03081f32afa033b1e9bfb88a7
sereda_pavel <assakay92@gmail.com>
2023-12-21 17:53:36 +0200

    end

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
-// const textPath = document.querySelector('textPath');
-// textPath.setAttribute('startOffset', '0%'); // Начало текста с середины круга
 
 const burgerBody = document.getElementById('burger-body')
 const burgerBtn = document.getElementById('burgerBtn')
+const burgerLine = document.getElementById('burger-line')
 
 burgerBtn.addEventListener('click', function(){
     if(burgerBody.classList.contains('burger-active')){
         burgerBody.classList.remove('burger-active')
+        burgerLine.classList.remove('burger-line-active')
     }else{
         burgerBody.classList.add('burger-active')
+        burgerLine.classList.add('burger-line-active')
     }
 
 })
